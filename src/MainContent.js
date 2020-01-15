4af56e0fd4064c77477ffebea6af10e5a618e3c7
Masha Ikromova <mashikro@gmail.com>
2020-01-15 14:49:07 +0800

    Created maincontent func component

---
+++ src/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MainContent() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Do StUfF</p>
+            <input type="checkbox" />
+            <p>Do StUfF</p>
+            <input type="checkbox" />
+            <p>Do StUfF</p>
+        </div>
+    );
+}
+export default MainContent;
