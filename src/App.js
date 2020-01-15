e1027229430248f13d877921693f0d4c72343b11
Masha Ikromova <mashikro@gmail.com>
2020-01-15 14:50:08 +0800

    Added children component to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Navbar from "./Navbar";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
+
 function App() {
-  return <h1>Hello world bitch!</h1>;
+  return (
+    <div>
+      <Navbar />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
