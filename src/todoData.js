138fa34aac0002a036e56afd72091086f0bbf8e4
Masha Ikromova <mashikro@gmail.com>
2020-01-20 10:43:32 +0800

    Added some fake data. Normally our todo list items will be coming from db or api

---
+++ src/todoData.js
@@ -0,0 +1,33 @@
+const todoData = [
+    {
+        id: 1,
+        text: "Organize backpack",
+        completed: true
+    },
+
+    {
+        id: 2,
+        text: "Take out the trash",
+        completed: false
+    },
+
+    {
+        id: 3,
+        text: "Check email",
+        completed: true
+    },
+
+    {
+        id: 4,
+        text: "Study React JS",
+        completed: false
+    },
+
+    {
+        id: 5,
+        text: "Do some MWH",
+        completed: false
+    }
+];
+
+export default todoData;
