0161012e7b492ac819a32590929e36624cf0467a
Masha Ikromova <mashikro@gmail.com>
2020-01-20 11:04:40 +0800

    Added a mapping function to the MainContent component. This component now receives outside data and maps each piece of data to a new MainContent component.

--- src/MainContent.js
+++ src/MainContent.js
@@ -2,14 +2,20 @@ import React from "react";
 
 import TodoItem from "./TodoItem";
 
+//normally the data for TODO list would come from an API call
+import todoData from "./todoData";
+
 function MainContent() {
-    const fullName = "Aaron Iba";
-    return (
-        <div>
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-        </div>
-    );
+    const todoList = todoData.map(todo => {
+        return (
+            <TodoItem
+                key={todo.id}
+                text={todo.text}
+                completed={todo.completed}
+            />
+        );
+    });
+
+    return <div className="todo-list">{todoList}</div>;
 }
 export default MainContent;
