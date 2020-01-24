8351dbf69c6a9d4ae0130286f0d34199ebe33353
Masha Ikromova <mashikro@gmail.com>
2020-01-24 17:00:56 +0800

    Changed func component to a class-based component

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,21 +1,32 @@
 import React from "react";
 
+// getting the data
 import TodoItem from "./TodoItem";
 
 //normally the data for TODO list would come from an API call
 import todoData from "./todoData";
 
-function MainContent() {
-    const todoList = todoData.map(todo => {
-        return (
-            <TodoItem
-                key={todo.id}
-                text={todo.text}
-                completed={todo.completed}
-            />
-        );
-    });
+// function MainContent() {
+//     const todoList = todoData.map(todo => {
+//         return <TodoItem key={todo.id} todo={todo} />;
+//     });
 
-    return <div className="todo-list">{todoList}</div>;
+//     return <div className="todo-list"> {todoList}</div>;
+// }
+
+class MainContent extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        const todoList = todoData.map(todo => {
+            return <TodoItem key={todo.id} todo={todo} />;
+        });
+
+        return <div className="todo-list"> {todoList}</div>;
+    }
 }
+
 export default MainContent;
