6691d005b0527c34a118f5957aadcf0a4bf197ed
Masha Ikromova <mashikro@gmail.com>
2020-01-26 12:08:58 +0800

    got rid of the: You provided a  prop to a form field without an  handler. error. Added onChange event handler, doesnt change sttae yet

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+function handleCheck() {
+    console.log("Changed!");
+}
+
 function TodoItem(props) {
     return (
         <div className="todo-list">
-            <input type="checkbox" checked={props.todo.completed} />
+            <input
+                type="checkbox"
+                checked={props.todo.completed}
+                onChange={handleCheck}
+            />
             <p>{props.todo.text}</p>
         </div>
     );
